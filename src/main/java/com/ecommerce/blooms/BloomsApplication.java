e3465f7706229548246f116fdb48493f9e841c40
marko220991 <19marko91.ilic@gmail.com>
2025-02-23 20:12:33 +0100

    add initial configuration

---
+++ src/main/java/com/ecommerce/blooms/BloomsApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.blooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloomsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/blooms/BloomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.blooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
