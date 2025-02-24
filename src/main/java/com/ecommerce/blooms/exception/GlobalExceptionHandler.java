55f0ba83e648f7a58aa26b7271a8a96475b700ac
marko220991 <19marko91.ilic@gmail.com>
2025-02-24 21:39:30 +0100

    add exception handler

---
+++ src/main/java/com/ecommerce/blooms/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.ecommerce.blooms.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> entityNotFoundException(EntityNotFoundException ex, WebRequest request) {
+        ErrorResponse message = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+    }
+}
