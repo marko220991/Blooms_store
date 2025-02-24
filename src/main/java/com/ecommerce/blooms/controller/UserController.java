1f15551e616aa32d22db31b0740eb380474e60fc
marko220991 <19marko91.ilic@gmail.com>
2025-02-24 21:37:10 +0100

    add CRUD operations for model User

--- src/main/java/com/ecommerce/blooms/controller/UserController.java
+++ src/main/java/com/ecommerce/blooms/controller/UserController.java
@@ -3,6 +3,7 @@ package com.ecommerce.blooms.controller;
 import com.ecommerce.blooms.model.User;
 import com.ecommerce.blooms.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,8 +21,25 @@ public class UserController {
         return userService.getAllUsers();
     }
 
-    @GetMapping("/id")
+    @GetMapping("/{id}")
     public Optional<User> getUserById(@PathVariable Long id) {
         return userService.getUserById(id);
+    }
+
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User addNewUser(@RequestBody User user) {
+        return userService.addNewUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+
+    @PutMapping("update/{id}")
+    public Optional<User> updateUser(@RequestBody User user,@PathVariable Long id) {
+        return userService.updateUser(user, id);
     }
 }
--- src/main/java/com/ecommerce/blooms/service/UserService.java
+++ src/main/java/com/ecommerce/blooms/service/UserService.java
@@ -1,5 +1,6 @@
 package com.ecommerce.blooms.service;
 
+import com.ecommerce.blooms.exception.EntityNotFoundException;
 import com.ecommerce.blooms.model.User;
 import com.ecommerce.blooms.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,34 @@ public class UserService {
     }
 
     public Optional<User> getUserById(Long id) {
-        return userRepository.findById(id);
+        return Optional.ofNullable(userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User with id " + id + " does not exist!")));
+    }
+
+    public User addNewUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()) {
+            throw new EntityNotFoundException("User with id " + id + " does not exist!");
+        }
+        userRepository.deleteById(id);
+    }
+
+    public Optional<User> updateUser(User user, Long id) {
+        Optional<User> userToUpdate = userRepository.findById(id);
+        if (userToUpdate.isEmpty()) {
+            throw new EntityNotFoundException("User with id " + id + " does not exist!");
+        }
+        User updatedUser = userToUpdate.get();
+
+        updatedUser.setName(user.getName());
+        updatedUser.setEmail(user.getEmail());
+        updatedUser.setUsername(user.getUsername());
+        updatedUser.setPassword(user.getPassword());
+
+        return Optional.of(userRepository.save(updatedUser));
     }
 }
